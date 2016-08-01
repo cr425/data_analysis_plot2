0466ed5a3f6c80daefd105105a93162bffb1ad6f
cr425 <cr425@cornell.edu>
2016-08-01 16:52:04 -0400

    including more graphs for final challenge

--- ownership_py_script.py
+++ ownership_py_script.py
@@ -1,26 +1,49 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 #Do different home_ownerships apply for more loan_amnt?
 
-data = pd.read_csv('LoanStats3a.csv')
-index = ['NONE','RENT', 'OWN', 'MORTGAGE', 'OTHER']
+data = pd.read_csv('../LoanStats3a.csv')
+#index = ['NONE','RENT', 'OWN', 'MORTGAGE', 'OTHER']
+#mapping = pd.Series([1,2,3,4,5], index)
+#x = data['home_ownership'].map(mapping)
+#plt.hist(x.dropna()) 
+#
+#plt.xlabel("Home Ownership")
+#plt.ylabel("No. of Loans")
+#plt.title("What Ownerships Borrow the Most?")
+#
+#axes = plt.gca()
+#axes.set_xticks([i for i in range(1,6)])
+#axes.set_xticklabels(index)
+#
+#fig = plt.gcf()
+#fig.set_size_inches(10, 10, forward=True)
+#fig.savefig('homeownership.png', dpi=100)
+#
+#plt.show()
 
-mapping = pd.Series([1,2,3,4,5], index)
-data['home_ownership_int'] = data['home_ownership'].map(mapping)
-home_ownership_int = data['home_ownership_int']
+#explore relationship for renters, owners, and mortgage holders
+df2 = data[['loan_amnt', 'home_ownership']]
+df2 = df2.dropna()
+#y = df2['loan_amnt']
+#x = df2['total_acc']
+#plt.scatter(x,y, marker="o")
+#fig = plt.gcf()
+#fig.set_size_inches(10, 10, forward=True)
+#fig.savefig('totalacc.png', dpi=100)
+#axes = plt.gca()
+#axes.set_xticks(x)
+#axes.set_xticks([i for i in range(1,60)])
+#axes.set_ylim([min(y),max(y)])
+#axes.set_xlim([0,60])
 
-x = home_ownership_int
-y = data['loan_amnt']
-plt.bar(x,y, width = .2, color = "blue")
-plt.xlabel("Home Ownership")
-plt.ylabel("Loan Amount")
-plt.title("What Ownerships Borrow the Most?")
-axes = plt.gca()
-axes.set_ylim([min(y),max(y)])
-axes.set_xticks([i for i in range(1,6) ])
-axes.set_xticklabels(index)
+#plt.hist(x,bins=20)
+plt.show()
 
-fig = plt.gcf()
-fig.set_size_inches(10, 10, forward=True)
-fig.savefig('homeownership.png', dpi=100)
+#grouping attempt
+bygroup_loans = df2.groupby(['home_ownership'])
+bygroup_loans.agg([np.sum, np.mean, np.std, len])
+
+
